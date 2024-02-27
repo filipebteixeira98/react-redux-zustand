3183d5d37eaa2adb01e652f418cdd32d40dc0b78
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-02-27 19:52:26 -0300

    chore: create home page default structure with header component, configure lucide-react icons library and change fonts to be antialiased

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,7 +1,25 @@
+import { MessageCircle } from 'lucide-react'
+
 export function Player() {
   return (
     <>
-      <div>Player</div>
+      <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
+        <div className="flex w-[1100px] flex-col gap-6">
+          <div className="flex items-center justify-between">
+            <div className="flex flex-col gap-1">
+              <h1 className="text-2xl font-bold">Redux Fundamentals</h1>
+              <span className="text-sm text-zinc-400">
+                Module "unraveling the redux"
+              </span>
+            </div>
+            <button className="flex items-center gap-2 rounded bg-violet-500 px-3 py-2 text-sm font-medium text-white hover:bg-violet-600">
+              <MessageCircle className="w-4 h-4" />
+              Leave feedback
+            </button>
+          </div>
+          <main></main>
+        </div>
+      </div>
     </>
   )
 }

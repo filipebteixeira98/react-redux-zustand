33421b47f5611ac6330006e556c83ce6ea573734
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-02-28 21:24:48 -0300

    feat(app): add functionality that allows users to select current module lesson

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,8 +1,11 @@
+import { useDispatch } from 'react-redux'
 import * as Collapsible from '@radix-ui/react-collapsible'
 import { ChevronDown } from 'lucide-react'
 
-import { Lesson } from './Lesson'
 import { useAppSelector } from '../store'
+import { play } from '../store/slices/player'
+
+import { Lesson } from './Lesson'
 
 interface ModuleProps {
   moduleIndex: number
@@ -14,6 +17,8 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
   const lessons = useAppSelector((state) => {
     return state.player.course.modules[moduleIndex].lessons
   })
+
+  const dispatch = useDispatch()
 
   return (
     <Collapsible.Root className="group">
@@ -31,12 +36,13 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
       </Collapsible.Trigger>
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-          {lessons.map((lesson) => {
+          {lessons.map((lesson, lessonIndex) => {
             return (
               <Lesson
                 key={lesson.id}
                 title={lesson.title}
                 duration={lesson.duration}
+                onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
               />
             )
           })}
